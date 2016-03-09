ffbe9cc9265ac97af0e6446c74ddc1fff733726e
harrypower <philipkingsmith@gmail.com>
2016-03-08 17:04:16 -0700

    no idea what i am doing now

--- sudoku.fs
+++ sudoku.fs
@@ -19,7 +19,6 @@
 require random.fs
 
 9 9 * cell * constant datasize
-9 cell * constant horzdatasize
 
 0 value sudokulist \ making space for the sudoku data
 here to sudokulist
@@ -27,13 +26,6 @@ datasize allot
 : clearsudoku ( -- )
    sudokulist datasize erase ; \ ensure starting with no values in the sudoku
 clearsudoku
-
-0 value horizontal-list
-here to horizontal-list
-horzdatasize allot
-: clearhorz ( -- ) \ erase horizontal-list
-   horizontal-list horzdatasize erase ;
-clearhorz
 
 : horzaddr ( nhorz nindex -- naddr ) \ calculate the address of horizontal data
    swap 9 * + ;
@@ -139,18 +131,6 @@ clearhorz
             j i sudoku@ \ if 0 is in sudokulist then nothing was stored there yet so repeat until something stored
          until
       loop
-   loop ;
-
-: makehorizontal ( nhorz -- ) \ will return when i has a solution to horizontal part of sudoku
-   clearhorz
-   9 0 do
-
-   loop ;
-
-: makesudoku2 ( -- )
-   clearsudoku
-   9 0 do
-      i makehorizontal
    loop ;
 
 : displaysudoku ( -- )
